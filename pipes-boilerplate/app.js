5a7e5a05489cb04e76085a3f10889e6bcdd2c692
cssher <sherali2910.sa@gmail.com>
2021-07-26 00:50:05 +0530

    added routing boilerplate

--- pipes-boilerplate/app.js
+++ pipes-boilerplate/app.js
@@ -1,12 +1,10 @@
 var http = require("http");
-
 var fs = require("fs");
 
 var server = http.createServer((req, res) => {
-    
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    var myReadStream = fs.createReadStream(__dirname + "/read.txt", "utf8");
-    myReadStream.pipe(res);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  var myReadStream = fs.createReadStream(__dirname + "/read.txt", "utf8");
+  myReadStream.pipe(res);
 });
 
 server.listen(3000, "127.0.0.1");
